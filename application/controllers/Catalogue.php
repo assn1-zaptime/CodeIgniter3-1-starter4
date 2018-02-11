d355219184c5671e49dd3253c5d06cfb3c8a358b
jade303 <tomjmckay@hotmail.ca>
2018-02-11 13:08:16 -0800

    controllers are case-sensitive!

--- application/controllers/Catalogue.php
+++ application/controllers/Catalogue.php
@@ -11,7 +11,7 @@ class Catalogue extends Application
 
     public function index()
     {
-        $this->data['pagebody'] = 'Catalogue';
+        $this->data['pagebody'] = 'catalogue';
         $this->render();
     }
 
