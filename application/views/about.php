d2fc3ce3da6c40011a4836c8882d38b49819d303
Alisa <Alisa@192.168.0.4>
2018-02-11 21:43:40 -0800

    fix the about page

--- application/views/about.php
+++ application/views/about.php
@@ -1,11 +1,8 @@
 <div id="content">
     <h1>About This WebSite</h1>
-    <p>This is a really simple website about face creator.</p>
-    <p>Thin there all come from the Internet <a href="http://images.google.ca" target="_blank">Google</a>.</p>
-    <h4>The Images</h4>
-    <p>Images are stored in a "img" folder inside the document root, and they are described in several csv files, which
-        works like the database.</p>
-    <h4>The Story</h4>
+    <p>This is a really simple face creator website.</p>
+    <p>The images are in the public domain. <a href="http://images.google.ca" target="_blank">Google</a>.</p>
+    <p>Created for educational use only.</p>
     <p>This website was created for the Spring 2018 session of COMP4711, at <a href="http://www.bcit.ca" target="_blank">BCIT</a>.</p>
     <p>The name of group and group memebers are in the footer.</p>
 </div>
